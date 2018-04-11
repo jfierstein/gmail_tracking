37903a1ab8b2700a5212f24b04da9fa4d4c44438
Josh Fierstein <josh.fierstein@turner.com>
2018-04-11 02:33:39 -0400

    add more stats

--- server/app.js
+++ server/app.js
@@ -8,11 +8,21 @@ const port = 3189;
 const logger = bunyan.createLogger({name: 'gmail-tracker'});
 app.set('trust proxy');
 const processMessage = (req, res, next) => {
-  const { id } = req.query;    
+  const { id } = req.query; 
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   try {
     let messages = JSON.parse(fs.readFileSync(`${__dirname}/data/messages.json`, 'utf8'));
-    let count = messages[id] || 0;
-    messages[id] = count+1;
+    const now = new Date(Date.now());
+    if(messages[id] && messages[id].lastUpdated ) {
+      const lastUpdated = new Date(messages[id].lastUpdated);
+      const minSinceLast = Math.abs(now - lastUpdated) / 1000;
+      if(minSinceLast > 1) {
+        messages[id].count += 1;
+        if(!messages[id].ips.includes(ip)) messages[id].ips.push(ip);
+        messages[id].lastUpdated = now.toISOString();
+      }
+    }
+    else messages[id] = { count: 0, lastUpdated: now.toISOString(), ips: [ip] }
     fs.writeFileSync(`${__dirname}/data/messages.json`, JSON.stringify(messages), 'utf8');
     return next();
   }
