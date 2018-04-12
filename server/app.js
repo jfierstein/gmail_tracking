27f5af65a8ada1da862e36fe5d9feb61f679db62
Josh Fierstein <josh.fierstein@turner.com>
2018-04-11 20:30:34 -0400

    env var for port

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bunyan = require('bunyan');
 const fs = require('fs');
 const app = express();
-const port = 3189;
+const port = process.env.PORT || 3189;
 const logger = bunyan.createLogger({ name: 'gmail-tracker' });
 app.set('trust proxy');
 
